20bb0d1c0504b9b2bee3e7d528fe7aa601e5bea1
DannyBoris <dannybo@affilomania.com>
2024-08-11 00:23:20 +0700

    remove login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,8 +12,8 @@ import { toast } from "react-toastify";
 
 function Login() {
   const { setUser } = useContext(UserContext);
-  const [email, setEmail] = useState("dannyboris1993@gmail.com");
-  const [password, setPassword] = useState("tpifSP1062016");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isPasswordVisible, setPasswordVisible] = useState(false);
 
   const [errors, setErrors] = useState({
